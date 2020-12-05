381c916e24a01dd558265aaf50c442df6a39b668
zaunermax <zaunermax@gmail.com>
2020-12-05 23:11:21 +0100

    feat(day1): finish day one

--- src/1/index.ts
+++ src/1/index.ts
@@ -1,20 +1,42 @@
-import { forkJoin, from, of } from 'rxjs';
-import {map, tap, timeInterval} from 'rxjs/operators';
+import { combineLatest, forkJoin, from, of } from 'rxjs';
+import { map, timeInterval, find, filter, mergeMap, tap } from 'rxjs/operators';
 import { readInput } from '../utils';
 
 const prepareInput = (rawInput: string) => rawInput.split(/\n/);
 
-const input$ = of(readInput()).pipe(map(prepareInput))
+const parseIntDec = (num: string) => Number.parseInt(num, 10);
 
-const goA = async (input: typeof input$) => {
-	return from(input)
-		.pipe(tap((test) => console.log(test)))
-		.toPromise();
-};
+const mapToNumberArray = (nums: string[]) => nums.map(parseIntDec);
 
-const goB = async (input: typeof input$) => {
-	return;
-};
+const input$ = of(readInput()).pipe(map(prepareInput), map(mapToNumberArray));
+
+const goA = (input: typeof input$) =>
+	input.pipe(
+		mergeMap((input) =>
+			from(input).pipe(
+				mergeMap((val) => combineLatest([of(val), from(input)])),
+				find(([a, b]) => a + b === 2020),
+				filter((val): val is [number, number] => !!val && !!val[0] && !!val[1]),
+				map(([a, b]) => a * b),
+			),
+		),
+	);
+
+const goB = (input: typeof input$) =>
+	input.pipe(
+		mergeMap((input) =>
+			from(input).pipe(
+				mergeMap((val) => combineLatest([of(val), from(input)])),
+				mergeMap(([a, b]) => combineLatest([of(a), of(b), from(input)])),
+				find(([a, b, c]) => a + b + c === 2020),
+				filter(
+					(val): val is [number, number, number] =>
+						!!val && !!val[0] && !!val[1] && !!val[2],
+				),
+				map(([a, b, c]) => a * b * c),
+			),
+		),
+	);
 
 forkJoin({ a: goA(input$), b: goB(input$) })
 	.pipe(timeInterval())
